ac240e0188ae7757147e3214d75be012a0ed32be
caua theodoro <caua.t.souza6@aluno.senai.br>
2025-11-25 14:57:52 -0300

    adicionar função calcular total

--- main.py
+++ main.py
@@ -16,3 +16,9 @@ if opcao == 1:
     desconto = float(input("Digite o valor do desconto: R$ "))
     resultado = funcoes.aplicar_desconto(valor, desconto)
     print("Valor final:", funcoes.formatar_moeda(resultado))
+
+elif opcao == 2:
+    valor = float(input("Digite o valor da compra: R$ "))
+    frete = float(input("Digite o valor do frete: R$ "))
+    resultado = funcoes.calcular_total(valor, frete)
+    print("Valor total:", funcoes.formatar_moeda(resultado))
